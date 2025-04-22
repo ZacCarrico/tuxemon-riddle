39d99a464527ffe29cd43a3ae9c3f0ebbfcbec7f
Jaskrendix <tur3a.giorgio.menzi@gmail.com>
2025-04-22 14:11:22 +0200

    play tile animation

--- tuxemon/event/actions/play_tile_animation.py
+++ tuxemon/event/actions/play_tile_animation.py
@@ -7,7 +7,6 @@ from dataclasses import dataclass
 from typing import final
 
 from tuxemon.animation_entity import setup_and_play_animation
-from tuxemon.event import get_npc
 from tuxemon.event.eventaction import EventAction
 from tuxemon.states.world.worldstate import WorldState
 
@@ -16,41 +15,36 @@ logger = logging.getLogger(__name__)
 
 @final
 @dataclass
-class PlayMapAnimationAction(EventAction):
+class PlayTileAnimationAction(EventAction):
     """
-    Trigger a map animation at a specified position based on the character's
-    coordinates within the world map
+    Trigger a map animation at a specified tile position on the world map.
 
     Script usage:
         .. code-block::
 
-            play_map_animation <animation_name>,<duration>,<loop>,<character>
+            play_tile_animation <tile_pos_x>,<tile_pos_y>,<animation_name>,<duration>,<loop>
 
     Script parameters:
+        tile_pos_x, tile_pos_y: Coordinates (x, y) specifying the tile position
+            where the animation will be drawn on the map.
         animation_name: The name of the animation stored in the
             resources/animations/tileset directory. For example, an animation
             named "grass" will load frames named "grass_xx.png".
         frame_duration: Duration (in seconds) for each frame of the animation.
         loop_mode: Indicates whether the animation should loop. Options: "loop"
             or "noloop".
-        character: Either "player" or character slug name (e.g. "npc_maple").
     """
 
-    name = "play_map_animation"
+    name = "play_tile_animation"
+    tile_pos_x: int
+    tile_pos_y: int
     animation_name: str
     duration: float
     loop: str
-    character: str
 
     def start(self) -> None:
-        character = get_npc(self.session, self.character)
-
-        if character is None:
-            logger.error(f"Character '{self.character}' not found")
-            return
-
         world_state = self.session.client.get_state_by_name(WorldState)
-        position = character.tile_pos
+        position = (self.tile_pos_x, self.tile_pos_y)
         animations = world_state.map_renderer.map_animations
 
         setup_and_play_animation(
